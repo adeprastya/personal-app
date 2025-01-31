7b2138c9e3343ccc387d271b78c9bc7ed62736c5
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2025-01-31 11:38:59 +0700

    remove jsonwebtoken and use jose

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,15 +1,19 @@
-import jwt from "jsonwebtoken";
-import { jwtVerify } from "jose";
+import { JWTPayload, SignJWT, jwtVerify } from "jose";
 import { CustomErrorResponse } from "./CustomErrorResponse";
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 if (!JWT_SECRET_KEY) {
 	throw new Error("Environment variable JWT_SECRET_KEY is not defined");
 }
+const secret = new TextEncoder().encode(JWT_SECRET_KEY);
 
 export const generateToken = async (data: string | Buffer | object) => {
 	try {
-		const token = jwt.sign(data, JWT_SECRET_KEY, { algorithm: "HS256", expiresIn: "1d" });
+		const token = await new SignJWT(data as JWTPayload)
+			.setProtectedHeader({ alg: "HS256", typ: "JWT" })
+			.setIssuedAt()
+			.setExpirationTime("1d")
+			.sign(secret);
 		return token;
 	} catch (err) {
 		throw new CustomErrorResponse(500, "Failed generating token", err);
@@ -17,8 +21,6 @@ export const generateToken = async (data: string | Buffer | object) => {
 };
 
 export const decodeToken = async (token: string) => {
-	const secret = new TextEncoder().encode(JWT_SECRET_KEY);
-
 	try {
 		const { payload } = await jwtVerify(token, secret);
 		return payload;
