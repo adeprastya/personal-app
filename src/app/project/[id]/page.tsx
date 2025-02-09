889ed1c6652d69334606597ade4b7fc4fb672fce
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2025-02-09 15:14:39 +0700

    add edit content in ProjectDetailPage

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -3,7 +3,9 @@
 import type { Project } from "@/types/Project";
 import { useParams } from "next/navigation";
 import Image from "next/image";
-import useFetch from "@/hooks/useFetch";
+import Link from "next/link";
+import useFetch, { axiosFetch } from "@/hooks/useFetch";
+import { timestampToReadable } from "@/utils/helper";
 
 export default function ProjectDetailPage() {
 	const { id } = useParams();
@@ -11,30 +13,149 @@ export default function ProjectDetailPage() {
 		method: "GET",
 		url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/project/${id}`
 	});
+	const project = data?.data;
+
+	const handleUpdate = async (e: React.FocusEvent<HTMLElement>, field: string) => {
+		if (e.target.textContent == "" || e.target.textContent == project?.[field as keyof Project]) {
+			return;
+		}
+
+		const { data, error } = await axiosFetch({
+			method: "PATCH",
+			url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/project/${id}`,
+			data: { [field]: e.target.textContent }
+		});
+
+		if (error) return console.error(error);
+
+		console.log(data);
+		refetch();
+	};
 
 	return (
-		<main className="w-full min-h-dvh p-8 flex flex-col gap-8">
-			<Image src={data?.data?.image_url || ""} alt={data?.data?.title || ""} width={800} height={300} />
+		<main className="w-full min-h-dvh flex items-center justify-center">
+			{/* Back button */}
+			<Link
+				href={"/project"}
+				className="fixed top-3 left-3 w-8 aspect-square rounded-sm bg-neutral-800 text-neutral-200 flex justify-center items-center"
+			>
+				{"<<"}
+			</Link>
 
-			<h1 className="text-2xl">{data?.data?.title}</h1>
+			{/* Main Content */}
+			{!loading && !error && (
+				<div className="p-8 flex flex-col gap-6">
+					<div className="flex flex-col gap-2">
+						<p className="text-xs tracking-wide">{timestampToReadable(project?.created_at as string)}</p>
 
-			<p>{data?.data?.description}</p>
+						{/* Title */}
+						<h1
+							contentEditable
+							suppressContentEditableWarning
+							spellCheck={false}
+							onBlur={(e) => handleUpdate(e, "title")}
+							title={project?.id}
+							className="font-semibold text-4xl"
+						>
+							{project?.title}
+						</h1>
 
-			<div>
-				{data?.data?.technologies.map((tech, i) => (
-					<span key={i}>{tech}, </span>
-				))}
-			</div>
+						<div className="flex flex-row flex-wrap gap-3">
+							{project?.technologies.map((tech, i) => (
+								<p key={i} className="px-2 py-0.5 rounded-md bg-neutral-200 tracking-wider text-xs">
+									{tech}
+								</p>
+							))}
+						</div>
+					</div>
 
-			<p>{data?.data?.site_url}</p>
+					<p
+						contentEditable
+						suppressContentEditableWarning
+						spellCheck={false}
+						onBlur={(e) => handleUpdate(e, "description")}
+						className="block max-w-3xl text-base"
+					>
+						{project?.description}
+					</p>
 
-			<p>{data?.data?.source_code_url}</p>
+					<div className="flex flex-col gap-2">
+						{project?.site_url && (
+							<div className="flex gap-2 items-center">
+								<p className="text-xs">Site: </p>
+								<p
+									contentEditable
+									suppressContentEditableWarning
+									spellCheck={false}
+									onBlur={(e) => handleUpdate(e, "site_url")}
+									className="text-sm"
+								>
+									{project.site_url}
+								</p>
+								<a
+									href={project.site_url}
+									target="_blank"
+									className="text-xs text-blue-500 underline underline-offset-2"
+								>
+									Visit
+								</a>
+							</div>
+						)}
 
-			<p>{data?.data?.demo_url}</p>
+						{project?.source_code_url && (
+							<div className="flex gap-2 items-center">
+								<p className="text-xs">Source Code: </p>
+								<p
+									contentEditable
+									suppressContentEditableWarning
+									spellCheck={false}
+									onBlur={(e) => handleUpdate(e, "source_code_url")}
+									className="text-sm"
+								>
+									{project.source_code_url}
+								</p>
+								<a
+									href={project.source_code_url || ""}
+									target="_blank"
+									className="text-xs text-blue-500 underline underline-offset-2"
+								>
+									Visit
+								</a>
+							</div>
+						)}
 
-			<p>{data?.data?.created_at}</p>
+						{project?.demo_url && (
+							<div className="flex gap-2 items-center">
+								<p className="text-xs">Demo: </p>
+								<p
+									contentEditable
+									suppressContentEditableWarning
+									spellCheck={false}
+									onBlur={(e) => handleUpdate(e, "demo_url")}
+									className="text-sm"
+								>
+									{project.demo_url}
+								</p>
+								<a
+									href={project.demo_url || ""}
+									target="_blank"
+									className="text-xs text-blue-500 underline underline-offset-2"
+								>
+									Visit
+								</a>
+							</div>
+						)}
+					</div>
 
-			<p>{data?.data?.id}</p>
+					<Image
+						src={project?.image_url || ""}
+						alt={project?.title || ""}
+						width={800}
+						height={300}
+						className="w-xl aspect-video object-cover rounded-sm"
+					/>
+				</div>
+			)}
 		</main>
 	);
 }
